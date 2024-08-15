702bba941ae1b1c25ce802dbc1ef599743b7a1f4
Samuel Parker <email@samuelparker.tech>
2024-08-15 16:48:43 +0000

    now saved changes should load models, form.py, views

--- index/models.py
+++ index/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Takeaway(models.Model):
+    """
+    Stores a single takeaway on the below details.
+    """
+    takeaway_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    food_type = models.CharField(max_length=50, unique=False)
+    address = models.CharField(max_length=50)
+    content = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["takeaway_name"]
+
+
+class Review(models.Model):
+    """
+    Stores/Adds a review to the takeaway post
+    """
+    takeaway_name = models.ForeignKey(
+        Takeaway, on_delete=models.CASCADE, related_name="reviews"
+    )
+    review_title = models.CharField(max_length=50, unique=False)
+    review_content = models.TextField()
+    poster = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="reviewer"
+    )
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        default=3
+    )
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    edited_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Review of {self.takeaway_name} by {self.poster}"
--- index/views.py
+++ index/views.py
@@ -1,7 +1,112 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import (
+    render, get_object_or_404, redirect, reverse
+)
+from django.views.generic import TemplateView
+from django.views import generic
+from .models import Takeaway, Review
+from .forms import ReviewForm
+from django.contrib import messages
+from django.db.models import Avg
+
 
 # Create your views here.
 
 def display_index(request):
     return HttpResponse("Hello World!")
+
+
+class TakeawayList(generic.ListView):
+    queryset = Takeaway.objects.all()
+    template_name = "index.html"
+
+    def get_queryset(self):
+        # Annotate each takeaway with its average rating
+        return Takeaway.objects.filter(status=1).annotate(
+            average_rating=Avg('reviews__rating')
+        ).order_by('Takeaway_name')
+
+
+def takeaway_detail(request, slug):
+    queryset = Takeaway.objects.filter(status=1)
+    takeaway = get_object_or_404(queryset, slug=slug)
+    reviews = takeaway.reviews.all().order_by("-created_on")
+    reviews_count = takeaway.reviews.filter(approved=True).count()
+    average_rating = reviews.aggregate(Avg('rating'))['rating__avg']
+
+    if request.method == "POST":
+        review_form = ReviewForm(data=request.POST)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.poster = request.user
+            review.review_title = review_form.cleaned_data.get('review_title')
+            review.review_content = review_form.cleaned_data.get(
+                'review_content'
+            )
+            review.rating = review_form.cleaned_data.get('rating')
+            review.takeaway_name = takeaway
+            review.save()
+            messages.add_message(
+                request, messages.SUCCESS, 'Review has been submitted'
+            )
+
+            return redirect('index', slug=slug)
+    else:
+        review_form = ReviewForm()
+
+    return render(
+        request,
+        "index.html",
+        {
+            "takeaway": takeaway,
+            "reviews": reviews,
+            "reviews_count": reviews_count,
+            "review_form": review_form,
+            "average_rating": average_rating,
+        },
+    )
+
+
+def review_edit(request, slug, review_id):
+    if request.method == "POST":
+        queryset = Takeaway.objects.filter(status=1)
+        takeaway = get_object_or_404(queryset, slug=slug)
+        review = get_object_or_404(Review, pk=review_id)
+        review_form = ReviewForm(data=request.POST, instance=review)
+
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            review.poster = request.user
+            review.review_title = review_form.cleaned_data.get(
+                'review_title'
+            )
+            review.review_content = review_form.cleaned_data.get(
+                'review_content'
+            )
+            review.rating = review_form.cleaned_data.get('rating')
+            review.takeaway_name = takeaway
+            review.save()
+            messages.add_message(
+                request, messages.SUCCESS, 'Review edited successfully'
+            )
+        else:
+            messages.add_message(
+                request, messages.ERROR, 'Error updating Review!'
+            )
+    return HttpResponseRedirect(reverse('index', args=[slug]))
+
+
+def review_delete(request, slug, review_id):
+    queryset = Takeaway.objects.filter(status=1)
+    takeaway = get_object_or_404(queryset, slug=slug)
+    review = get_object_or_404(Review, pk=review_id)
+
+    if review.poster == request.user:
+        review.delete()
+        messages.add_message(request, messages.SUCCESS, 'Review deleted!')
+    else:
+        messages.add_message(
+            request, messages.ERROR, 'You can only delete your own reviews!'
+        )
+
+    return HttpResponseRedirect(reverse('index', args=[slug]))
