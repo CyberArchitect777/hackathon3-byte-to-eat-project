026d78b4ff2132a0e2607742b887acbe0e5b2180
Samuel Parker <email@samuelparker.tech>
2024-08-15 15:44:54 +0000

    instances of 'Beer' replaced with takeaway

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,17 @@
+
 from django.contrib import admin
+from .models import Takeaway, Review
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Takeaway)
+class TakeawayAdmin(SummernoteModelAdmin):
+    list_display = ('takeaway_name', 'slug', 'status')
+    search_fields = ['takeaway_name', 'content', ]
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('takeaway_name',)}
+    summernote_fields = ('content',)
+
 
 # Register your models here.
+admin.site.register(Review)
