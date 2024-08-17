b1f93815a3a8de70258f43385ff43baec3e822c2
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-08-17 22:52:01 +0000

    removed profile page as new review page added

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.display_profile, name="display_profile"),
     path("review-dashboard/", views.review_dashboard, name="review_dashboard"),
 ]
