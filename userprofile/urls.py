c7ca7288735797fb6b9d322567621858682872c4
Christina Hughes <c.j.hughes@hotmail.com>
2024-08-17 18:00:28 +0000

    Fix type on urls.py - change - to _

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
     path("review_dashboard/", views.review_dashboard, name="review_dashboard"),
 
     # So User can add a new review
-    path("add_review/", views.add-review, name="add_review"),
+    path("add_review/", views.add_review, name="add_review"),
 
      # So User can edit their existing review
-    path("edit_review/<pk>/", views.edit-review, name="edit_review"),
+    path("edit_review/<pk>/", views.edit_review, name="edit_review"),
 
      # So User can delete their existing review
-    path("delete_review/<pk>/", views.delete-review, name="delete_review"),
+    path("delete_review/<pk>/", views.delete_review, name="delete_review"),
 ]
