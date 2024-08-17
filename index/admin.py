beb62ec426723b6f0bb41c158fb721af64cc6809
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-08-17 13:35:00 +0000

    removed takeaway model from admin.py

--- index/admin.py
+++ index/admin.py
@@ -4,13 +4,13 @@ from .models import Takeaway, Review
 from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.register(Takeaway)
-class TakeawayAdmin(SummernoteModelAdmin):
-    list_display = ('takeaway_name', 'slug', 'status')
-    search_fields = ['takeaway_name', 'content', ]
-    list_filter = ('status',)
-    prepopulated_fields = {'slug': ('takeaway_name',)}
-    summernote_fields = ('content',)
+# @admin.register(Takeaway)
+# class TakeawayAdmin(SummernoteModelAdmin):
+#     list_display = ('takeaway_name', 'slug', 'status')
+#     search_fields = ['takeaway_name', 'content', ]
+#     list_filter = ('status',)
+#     prepopulated_fields = {'slug': ('takeaway_name',)}
+#     summernote_fields = ('content',)
 
 
 # Register your models here.
