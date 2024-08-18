8d882de5dd492026c8648d5c8d0af653e55cdc71
“CyberArchitect777” <169656116+CyberArchitect777@users.noreply.github.com>
2024-08-18 00:18:46 +0000

    installed crispy forms

--- bytetoeat/settings.py
+++ bytetoeat/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'allauth',
 	'allauth.account',
 	'allauth.socialaccount',
+    'crispy_forms', 
+    'crispy_bootstrap5',
     'cloudinary_storage',
     'django_summernote',
     'cloudinary',
@@ -57,6 +59,10 @@ INSTALLED_APPS = [
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# This sets the bootstrap5 framework library as an allowed template pack and also sets it as the default
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
